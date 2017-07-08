beedb7be75d2fd257d9c6cc306f411db5c9bba71
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-07-08 15:46:50 +0200

    Fetch weather using axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = 'cfb4d891b7b3d60f811a67db16e4bc4d'; 
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather (city) {
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	}
+}
